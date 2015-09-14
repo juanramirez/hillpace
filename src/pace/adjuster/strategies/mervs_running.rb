686b32ec15e9ade5ca55931dac1847c1a0af2297
Juan Ramírez Ruiz <juan.ramirez.ruiz@gmail.com>
2015-09-14 02:12:12 +0200

    Fixed Merv's Running strategy.

--- src/pace/adjuster/strategies/mervs_running.rb
+++ src/pace/adjuster/strategies/mervs_running.rb
@@ -1,5 +1,5 @@
 # source: http://merv.stanford.edu/runcalc?Request=explanation
 MERVS_RUNNING = lambda do |pace, incline|
-  division_factor = 1.0 - (incline * 4.5 / 100.0)
+  division_factor = 1.0 - (incline * 4.5)
   Pace.from_seconds_per_km(pace.seconds_per_km / division_factor)
 end
