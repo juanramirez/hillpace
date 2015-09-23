84c87bc2127fd68a7c1b6faeaaa869cd08b89c8d
Juan Rami­rez Ruiz <juan.ramirez.ruiz@gmail.com>
2015-09-23 16:36:45 +0000

    #3 Documented GpxParser.

--- lib/hillpace/input/gpx/gpx_parser.rb
+++ lib/hillpace/input/gpx/gpx_parser.rb
@@ -4,22 +4,34 @@ require 'geoelevation'
 module Hillpace
   module Input
     module Gpx
+      # Parser for GPX files (see http://www.topografix.com/gpx.asp).
       class GpxParser
         attr_reader :document
 
+        # Initializes a GpxParser object.
+        # @param gpx_content [string] The content of a GPX file.
         def initialize(gpx_content)
           @document = Nokogiri::XML gpx_content
           @srtm = GeoElevation::Srtm.new
         end
 
+        # Creates a new GpxParser object from a GPX file path.
+        # @param gpx_path [string] The path of the file to be parsed.
+        # @return [GpxParser]
         def self.from_file(gpx_path)
           new File.open gpx_path
         end
 
+        # Creates a new GpxParser object from a GPX file URL.
+        # @param gpx_url [string] The URL of the file to be parsed.
+        # @return [GpxParser]
         def self.from_url(gpx_url)
           new open gpx_url
         end
 
+        # Parses a GPX document.
+        # Looks for route nodes inside the document and parses them.
+        # @return [Route]
         def parse
           routes = Array.new
           document.search('trk').each do |route_node|
@@ -30,6 +42,10 @@ module Hillpace
 
         private
 
+        # Parses a route node.
+        # Looks for segment nodes inside the route node and parses them.
+        # @param route_node [Nokogiri::XML::Node] The XML node corresponding to the route.
+        # @return [Route]
         def parse_route(route_node)
           segments = Array.new
           route_node.search('trkseg').each do |segment_node|
@@ -38,6 +54,10 @@ module Hillpace
           Route.new(segments)
         end
 
+        # Parses a segment node.
+        # Looks for track point nodes inside the segment node and parses them.
+        # @param segment_node [Nokogiri::XML::Node] The XML node corresponding to the segment.
+        # @return [Segment]
         def parse_segment(segment_node)
           kalman_filter = KalmanFilter.new
           track_points = Array.new
@@ -52,6 +72,16 @@ module Hillpace
           Segment.new track_points
         end
 
+        # Parses a track point node.
+        # Looks for the attributes of a track node (longitude, latitude and elevation) and creates a TrackPoint object
+        # accordingly.
+        #
+        # Note: These attributes don't need to be exactly the ones the GPX provides.
+        # For longitude and latitude, a Kalman filter is applied if the time is also provided.
+        # Also, the elevation is not got from the GPX, but deduced from longitude and latitude.
+        #
+        # @param track_point_node [Nokogiri::XML::Node] The XML node corresponding to the track point.
+        # @return [TrackPoint]
         def parse_track_point(track_point_node)
           longitude = (track_point_node ['lon']).to_f
           latitude = (track_point_node ['lat']).to_f
