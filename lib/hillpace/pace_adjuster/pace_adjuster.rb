146e77d16dea90a0011f9ae4b49d9fdd5573f680
Juan Ramírez Ruiz <juan.ramirez.ruiz@gmail.com>
2015-10-03 09:42:37 +0200

    #3 Documented PaceAdjuster class.

--- lib/hillpace/pace_adjuster/pace_adjuster.rb
+++ lib/hillpace/pace_adjuster/pace_adjuster.rb
@@ -2,11 +2,18 @@ require_relative '../pace'
 
 module Hillpace
   module PaceAdjuster
+
     class PaceAdjuster
+      # Initializes a PaceAdjuster object.
+      # @param strategy [Proc] The adjust strategy that the pace adjuster will use.
       def initialize(strategy)
         @strategy = strategy
       end
 
+      # Adjusts a reference pace depending on the incline.
+      # @param pace [Pace] A reference pace,
+      # @param incline [Number] An incline value.
+      # @return [Pace]
       def adjust_pace(pace, incline)
         @strategy.call pace, incline
       end
--- lib/hillpace/pace_adjuster/strategies/lets_run_forum.rb
+++ lib/hillpace/pace_adjuster/strategies/lets_run_forum.rb
@@ -1,7 +1,7 @@
 module Hillpace
   module PaceAdjuster
     module Strategies
-      #source: http://www.letsrun.com/forum/flat_read.php?thread=197366&page=0
+      # source: http://www.letsrun.com/forum/flat_read.php?thread=197366&page=0
       LETS_RUN_FORUM = lambda do |pace, incline|
         division_factor = 1.0 + (incline * 9)
         Hillpace::Pace.from_meters_per_second(pace.meters_per_second / division_factor)
